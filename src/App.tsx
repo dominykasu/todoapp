f7c6ffa7b8de9cacad080f7247b9fdd0a79e17c4
Dominykas Usevicius <dominykas.usevicius@gmail.com>
2025-02-05 10:04:33 +0200

    add localStorage persistence

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import {useState, useEffect} from "react"
 import React from 'react';
 import './App.css';
 import TodoInput from "./components/TodoInput";
@@ -6,31 +6,51 @@ import TodoList from "./components/TodoList";
 
 function App() {
 
-  const [todos, setTodos] = useState([])
-  const [todoValue, setTodoValue] = useState('')
+    const [todos, setTodos] = useState([])
+    const [todoValue, setTodoValue] = useState('')
 
-  const handleAddTodos = (newTodo) => {
-    const newTodoList = [...todos, newTodo];
-    setTodos(newTodoList);
-  }
-  const handleEditTodos = (index) => {
-    setTodoValue(todos[index])
-    handleDeleteTodos(index);
-  }
+    const persistData = (newList) => {
+        localStorage.setItem('todos', JSON.stringify({todos: newList}))
+    }
+    const handleAddTodos = (newTodo) => {
+        const newTodoList = [...todos, newTodo];
+        persistData(newTodoList);
+        setTodos(newTodoList);
+    }
+    const handleEditTodos = (index) => {
+        setTodoValue(todos[index])
+        handleDeleteTodos(index);
+    }
 
-  const handleDeleteTodos = (index) => {
-    const newTodoList = todos.filter((todo, todoIndex) => {
-      return todoIndex !== index;
-    })
-    setTodos(newTodoList);
-  }
+    const handleDeleteTodos = (index) => {
+        const newTodoList = todos.filter((todo, todoIndex) => {
+            return todoIndex !== index;
+        })
+        persistData(newTodoList);
+        setTodos(newTodoList);
+    }
 
-  return (
-  <>
-    <TodoInput handleAddTodos={handleAddTodos} todoValue={todoValue} setTodoValue={setTodoValue}/>
-    <TodoList todos={todos} handleDeleteTodos={handleDeleteTodos} handleEditTodos={handleEditTodos}/>
-  </>
-  );
+    useEffect(() => {
+        if (!localStorage) {
+            return;
+        }
+        let localTodos = localStorage.getItem('todos');
+        if (!localTodos) {
+            return;
+        }
+
+        if (localTodos) {
+            localTodos = JSON.parse(localTodos).todos
+            setTodos(localTodos)
+        }
+    }, [])
+
+    return (
+        <>
+            <TodoInput handleAddTodos={handleAddTodos} todoValue={todoValue} setTodoValue={setTodoValue}/>
+            <TodoList todos={todos} handleDeleteTodos={handleDeleteTodos} handleEditTodos={handleEditTodos}/>
+        </>
+    );
 }
 
 export default App;
